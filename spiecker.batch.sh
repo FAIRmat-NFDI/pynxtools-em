751191167dcc7d63192d63975cb02dc48f780986
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2024-04-08 14:31:04 +0200

    Init ipynb dev tools and file routining to enable parsing examples from E. Spiecker

---
+++ spiecker.batch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# call from within the top-level directory of pynxtools_em
+dsrc="../../../../paper_paper_paper/scidat_nomad_ebsd/bb_analysis/data/development_spiecker"
+esrc="examples"
+trg="debug"
+
+examples="1340 Camera Ceta 440 kx.emd"
+
+for example in $examples; do
+	echo $example
+	dataconverter convert $esrc/em.oasis.specific.yaml $esrc/eln_data.yaml $dsrc/$example --reader em --nxdl NXem --output=$trg/debug.$example.nxs 1>$trg/stdout.$example.nxs.txt 2>$trg/stderr.$example.nxs.txt
+done
