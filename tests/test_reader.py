c3914a49209ea4c47ae468799b801ab8c98fac0f
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2024-04-08 12:49:23 +0200

    Fixed location of deps for test

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1,6 +1,20 @@
-"""
-Basic example based test for the stm reader
-"""
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 import os
 import xml.etree.ElementTree as ET
@@ -11,7 +25,7 @@ from pynxtools.dataconverter.helpers import (
     validate_data_dict,
 )
 from pynxtools.dataconverter.template import Template
-from pynxtools.nexus.nxdl_utils import get_nexus_definitions_path
+from pynxtools.definitions.dev_tools.utils.nxdl_utils import get_nexus_definitions_path
 from pynxtools_em.reader import EMReader
 
 
