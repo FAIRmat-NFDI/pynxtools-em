720378a21ef7b8769a6854b338b61d59f4dec644
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2024-04-11 18:32:24 +0200

    Tested with all IKZ Kernke/Bergmann/Brückner Apreo images successfully

--- imgs.batch.sh
+++ imgs.batch.sh
@@ -10,12 +10,12 @@ trg="debug"
 # examples="kit/FeMoOx_AntiA_04_1k5x_CN.tif"
 
 # IKZ SEM TFS Apreo for all imaging modes
-examples="0c8nA_3deg_003_AplusB_test.tif"  # T1
+#examples="0c8nA_3deg_003_AplusB_test.tif"  # T1
 #examples="ALN_baoh_021.tif"  # T2
 #examples="T3_image.tif"  # T3
 #examples="ETD_image.tif"  # ETD
 #examples="NavCam_normal_vis_light_ccd.tif"  # NavCam
-#examples="0c8nA_3deg_003_AplusB_test.tif ALN_baoh_021.tif T3_image.tif ETD_image.tif NavCam_normal_vis_light_ccd.tif"
+examples="0c8nA_3deg_003_AplusB_test.tif ALN_baoh_021.tif T3_image.tif ETD_image.tif NavCam_normal_vis_light_ccd.tif"
 
 #examples="axon/20210426T224437.049Raw0.png"
 #examples="ReductionOfFeOx.zip"
