5fcfcf9c5b859bfc7a690337acc9bcc3eee04ee4
mkuehbach <markus.kuehbach@hu-berlin.de>
2024-05-25 17:21:44 +0200

    Code to select default plot such that if it is IPF and there are multiple phases showing for the phase the highest number of indexed scan points

--- pynxtools_em/utils/nx_default_plots.py
+++ pynxtools_em/utils/nx_default_plots.py
@@ -18,8 +18,13 @@
 """Logics and functionality to identify and annotate a default plot NXem."""
 
 from typing import Dict
-
+from operator import itemgetter
 import numpy as np
+
+
+def sort_list_of_tuples_desc_order(tuples):
+    """Sort the tuples by the second item using the itemgetter function."""
+    return sorted(tuples, key=itemgetter(1), reverse=True)
 
 
 class NxEmDefaultPlotResolver:
@@ -79,14 +84,43 @@ class NxEmDefaultPlotResolver:
 
             # find ebsd ipf map
             idx_head = key.find("/ROI[")
-            tail = "/ebsd/indexing/phaseID[phase1]/ipfID[ipf1]/map"
-            idx_tail = key.find(tail)
-            if idx_head is not None and idx_tail is not None:
-                if 0 < idx_head < idx_tail:
-                    keyword = key[0 : idx_tail + len(tail)]
-                    if keyword not in candidates[5]:
-                        candidates[5].append(keyword)
-                    continue
+            idx = key.find("/ebsd/indexing")
+            n_scan_points_total = 1.0
+            if (
+                f"{key[0:idx + len('/ebsd/indexing')]}/number_of_scan_points"
+                in template
+            ):
+                n_scan_points_total = template[
+                    f"{key[0:idx + len('/ebsd/indexing')]}/number_of_scan_points"
+                ]
+                # in case of ebsd map with phase2, phase3, ... find than phase with the
+                vote_ipf_map = []
+                phase_id = 1
+                while phase_id < 20:
+                    tail = "/ebsd/indexing/phaseID[phase{phase_id}]"  # ipfID[ipf1]/map"
+                    idx_tail = key.find(tail)
+                    prfx = f"{key[0:idx_tail + len(tail)]}"
+                    if idx_head is not None and idx_tail is not None:
+                        if 0 < idx_head < idx_tail:
+                            if f"{prfx}/ipfID[ipf1]/map/data" in template:
+                                n_scan_points = 1.0
+                                if f"{prfx}/number_of_scan_points" in template:
+                                    n_scan_points = template[
+                                        f"{key[0:idx_tail + len(tail)]}/number_of_scan_points"
+                                    ]
+                                vote_ipf_map.append(
+                                    (
+                                        f"{prfx}/ipfID[ipf1]/map",
+                                        np.float64(n_scan_points)
+                                        / np.float64(n_scan_points_total),
+                                    )
+                                )
+                    phase_id += 1
+                vote_ipf_map = sort_list_of_tuples_desc_order(vote_ipf_map)
+                if len(vote_ipf_map) > 0:
+                    if vote_ipf_map[0][0] not in candidates[5]:
+                        candidates[5].append(vote_ipf_map[0][0])
+
             # find ebsd roi map
             tail = "/ebsd/indexing/roi"
             idx_tail = key.find(tail)
