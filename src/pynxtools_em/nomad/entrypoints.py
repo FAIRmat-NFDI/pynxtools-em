8c9e2a52a0005c62904fb13370a4dc9db357dbc3
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2024-10-14 14:39:17 +0200

    typo fix

--- src/pynxtools_em/nomad/entrypoints.py
+++ src/pynxtools_em/nomad/entrypoints.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-"""Entry points for XPS examples."""
+"""Entry points for EM examples."""
 
 try:
     from nomad.config.models.plugins import ExampleUploadEntryPoint
