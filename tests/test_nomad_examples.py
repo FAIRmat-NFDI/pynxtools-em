b3ccd7670d70029abd9490e065b9a9dea616ac27
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2024-10-14 16:43:25 +0200

    return archive dict in testing function

--- tests/test_nomad_examples.py
+++ tests/test_nomad_examples.py
@@ -42,7 +42,7 @@ from pynxtools_em.nomad.entrypoints import em_example
 )
 def test_parse_nomad_examples(mainfile):
     """Test if NOMAD examples work."""
-    parse_nomad_examples(mainfile)
+    archive_dict = parse_nomad_examples(mainfile)
 
 
 @pytest.mark.parametrize(
