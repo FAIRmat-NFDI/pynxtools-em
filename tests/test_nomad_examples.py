fd58608204934a287daa1f641bea08b63612433a
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2024-10-15 09:58:42 +0200

    use relative path for test

--- tests/test_nomad_examples.py
+++ tests/test_nomad_examples.py
@@ -17,6 +17,7 @@
 #
 """Test for NOMAD examples in APM reader plugin."""
 
+import os
 import pytest
 
 try:
@@ -38,7 +39,17 @@ from pynxtools_em.nomad.entrypoints import em_example
 
 
 @pytest.mark.parametrize(
-    "mainfile", get_file_parameter("src/pynxtools_em/nomad/examples")
+    "mainfile",
+    get_file_parameter(
+        os.path.join(
+            os.path.dirname(__file__),
+            "..",
+            "src",
+            "pynxtools_em",
+            "nomad",
+            "examples",
+        )
+    ),
 )
 def test_parse_nomad_examples(mainfile):
     """Test if NOMAD examples work."""
