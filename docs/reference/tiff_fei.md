c2b24ad0afdc743b484e81b53ee674ae31aafa4d
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2024-10-26 01:10:01 +0200

    Configuration table and logic to identify Tecnai, next steps: i) test, ii) implement for Helios NanoLab, merge

---
+++ docs/reference/tiff_fei.md
@@ -0,0 +1,5 @@
+# FEI Legacy Tagged Image File Format TIFF
+
+The pynxtools-em parser and normalizer reads the following content and maps them on respective NeXus concepts that are defined in the NXem application definition:
+
+# TODO
---
+++ src/pynxtools_em/configurations/image_tiff_fei_cfg.py
@@ -0,0 +1,98 @@
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Configuration of the image_tiff_fei parser."""
+
+from typing import Any, Dict
+
+from pynxtools_em.utils.pint_custom_unit_registry import ureg
+
+# Tecnai TEM-specific metadata based on prototype example
+# currently not mapped:
+# Intensity____31.429 dimensionless
+# Objective lens____92.941 dimensionless
+# Diffraction lens____36.754 dimensionless
+# TODO::changeme need to go elsewhere when after the Autumn NIAC meeting NXem
+
+
+FEI_TECNAI_STATIC_VARIOUS_NX: Dict[str, Any] = {
+    "prefix_trg": "/ENTRY[entry*]/measurement/em_lab",
+    "prefix_src": "",
+    "use": [("fabrication/vendor", "FEI")],
+    "map": [
+        ("fabrication/model", "Microscope"),
+        ("ebeam_column/electron_source/emitter_type", "Gun type"),
+    ],
+}
+
+
+FEI_TECNAI_DYNAMIC_OPTICS_NX: Dict[str, Any] = {
+    "prefix_trg": "/ENTRY[entry*]/measurement/event_data_em_set/EVENT_DATA_EM[event_data_em*]/em_lab/OPTICAL_SYSTEM_EM[optical_system_em]",
+    "prefix_src": "",
+    "map": [("filtermode_changeme", "Filter mode")],
+    "map_to_u4": [("gunlens_changeme", "Gun lens"), ("spotsize_changeme", "Spot size")],
+    "map_to_f8": [
+        ("magnification", "Magnification"),
+        ("camera_length", ureg.meter, "Camera length", ureg.meter),
+        ("defocus", ureg.meter, "Defocus", ureg.micrometer),
+        ("stemrotation_changeme", ureg.radian, "Stem rotation", ureg.degree),
+        ("stemrotation_changeme", ureg.radian, "Stem rotation correction", ureg.degree),
+    ],
+}
+
+
+FEI_TECNAI_DYNAMIC_STAGE_NX: Dict[str, Any] = {
+    "prefix_trg": "/ENTRY[entry*]/measurement/event_data_em_set/EVENT_DATA_EM[event_data_em*]/em_lab/STAGE_LAB[stage_lab]",
+    "prefix_src": "",
+    "map_to_f8": [
+        ("tilt1", ureg.radian, "Stage A", ureg.degree),
+        ("tilt2", ureg.radian, "Stage B", ureg.degree),
+        (
+            "position",
+            ureg.meter,
+            ["Stage X", "Stage Y", "Stage Z"],
+            ureg.micrometer,
+        ),
+    ],
+}
+
+
+FEI_TECNAI_DYNAMIC_VARIOUS_NX: Dict[str, Any] = {
+    "prefix_trg": "/ENTRY[entry*]/measurement/event_data_em_set/EVENT_DATA_EM[event_data_em*]",
+    "prefix_src": "",
+    "map": [("em_lab/ebeam_column/operation_mode", "Mode")],
+    "map_to_f8": [
+        (
+            "em_lab/ebeam_column/electron_source/voltage",
+            ureg.volt,
+            "High tension",
+            ureg.kilovolt,
+        ),
+        (
+            "em_lab/ebeam_column/electron_source/extraction_voltage",
+            ureg.volt,
+            "Extraction voltage",
+            ureg.kilovolt,
+        ),
+        (
+            "em_lab/ebeam_column/electron_source/emission_current",
+            ureg.ampere,
+            "Emission",
+            ureg.microampere,
+        ),
+    ],
+}
