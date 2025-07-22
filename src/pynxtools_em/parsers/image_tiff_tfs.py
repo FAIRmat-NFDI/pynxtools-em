e7799b604942807f98b17f2370ec614a9a39afb9
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2025-07-22 11:19:37 +0200

    Fixing remaining image_tfs_* test cases

--- src/pynxtools_em/parsers/image_tiff_tfs.py
+++ src/pynxtools_em/parsers/image_tiff_tfs.py
@@ -209,8 +209,8 @@ class TfsTiffParser:
                 template[f"{trg}/real/@long_name"] = f"Real part of the image intensity"
 
                 sxy = {
-                    "i": ureg.Quantity(1.0, ureg.meter),
-                    "j": ureg.Quantity(1.0, ureg.meter),
+                    "i": ureg.Quantity(1.0),
+                    "j": ureg.Quantity(1.0),
                 }
                 # may face CCD overview camera of chamber that has no calibration!
                 if ("EScan/PixelWidth" in self.flat_dict_meta) and (
@@ -225,7 +225,7 @@ class TfsTiffParser:
                         ),
                     }
                 else:
-                    print("WARNING: Assuming pixel width and height unit is meter!")
+                    print("WARNING: Assuming pixel width and height unit is unitless!")
                 nxy = {"i": np.shape(np.array(fp))[1], "j": np.shape(np.array(fp))[0]}
                 # TODO::be careful we assume here a very specific coordinate system
                 # however the TIFF file gives no clue, TIFF just documents in which order
