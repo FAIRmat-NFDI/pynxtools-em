Fix habit that when no proper scaling is provided that the axis are decorated with the statement dimensionless which is incorrect but pixel is used now

src/pynxtools_em/parsers/hfive_apex.py
@@ -672,7 +672,7 @@ class HdfFiveEdaxApexParser(HdfFiveBaseParser):
                     np.uint32(dim_idx)
                 )
                 template[f"{trg}/image_2d/axis_{dim}/@long_name"] = (
-                    f"Point coordinate along the {dim}-axis ({qnt.units})"
+                    f"Coordinate along the {dim}-axis ({qnt.units})"
                 )
                 template[f"{trg}/image_2d/axis_{dim}/@units"] = f"{qnt.units}"
         if len(atom_types) > 0: