f740c913309d0d1f7555a2f444e7106afadd56b0
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2025-11-28 19:59:47 +0100

    Working in changes as per the cruft template, tests

---
+++ tests/test_nomad_apps.py
@@ -0,0 +1,35 @@
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Tests for the NOMAD app."""
+
+import pytest
+
+try:
+    import nomad  # noqa: F401
+except ImportError:
+    pytest.skip(
+        "Skipping NOMAD app tests because nomad-lab is not installed",
+        allow_module_level=True,
+    )
+
+
+def test_importing_app():
+    # this will raise an exception if pydantic model validation fails for the app
+    from pynxtools_em.nomad.apps import em_app_entry_point  # noqa: PLC0415
+
+    assert em_app_entry_point.app.label == "EM"
