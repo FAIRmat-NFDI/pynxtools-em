f2e98be3ee23ec49230a26c411b1ddf39aa17c57
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2025-12-03 20:20:44 +0100

    Batch processing skeleton

--- examples/ger_berlin_koch/ger_berlin_koch_batch_process.py
+++ examples/ger_berlin_koch/ger_berlin_koch_batch_process.py
@@ -25,3 +25,53 @@
 # during this processing technology-partner specific content,
 # e.g., nion, JEOL, Zeiss, stuff is parsed using pynxtools-em to generate
 # one NeXus/HDF5 file per project
+
+# python3 ger_berlin_koch_batch_process.py 'microscope_dir' '.'
+import os
+import sys
+import datetime
+import logging
+
+DEFAULT_LOGGER_NAME = "ger_berlin_koch_group_process"
+logger = logging.getLogger(DEFAULT_LOGGER_NAME)
+logging.basicConfig(
+    filename=f"{DEFAULT_LOGGER_NAME}.log",
+    filemode="w",  # use "a" to collect all in a session, use "w" to overwrite
+    format="%(levelname)s %(asctime)s %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%S%z",
+    encoding="utf-8",
+    level=logging.DEBUG,
+)
+
+microscope_directory = sys.argv[1]
+target_directory = sys.argv[2]
+tic = datetime.datetime.now().timestamp()
+
+logger.info(f"{tic}")
+logger.info(f"program name {__name__}")
+logger.info(f"python version {sys.version}")
+logger.info(f"working_directory {os.getcwd()}")
+logger.info(f"microscope_directory {microscope_directory}")
+logger.info(f"target_directory {target_directory}")
+
+cnt = 0
+byte_size_processed = 0
+for root, dirs, files in os.walk(microscope_directory):
+    for file in files:
+        fpath = f"{root}/{file}".replace(os.sep * 2, os.sep)
+        fname = os.path.basename(fpath)
+        cnt += 1
+        # if byte_size_processed >= (1 * 1024 * 1024 * 1024):  # incremental reporting
+        #    byte_size_processed = 0
+        # always attempt to hash the file first
+        try:
+            logger.info(f"{cnt}, {fname}")
+            # stat = os.stat(fpath)
+            # byte_size_processed += stat.st_size
+        except Exception as e:
+            logger.warning(f"{fpath}____{e}")
+            continue
+
+toc = datetime.datetime.now().timestamp()
+logger.info(f"{toc}")
+print(f"Batch queue processed successfully")
