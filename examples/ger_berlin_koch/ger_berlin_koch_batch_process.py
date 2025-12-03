0d58bb9fa3aa171d54a8e6d4afed3a96912b6f8d
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2025-12-03 20:33:43 +0100

    microseconds in the log, adding filesize

--- examples/ger_berlin_koch/ger_berlin_koch_batch_process.py
+++ examples/ger_berlin_koch/ger_berlin_koch_batch_process.py
@@ -32,13 +32,14 @@ import sys
 import datetime
 import logging
 
+SEPARATOR = "____"
 DEFAULT_LOGGER_NAME = "ger_berlin_koch_group_process"
 logger = logging.getLogger(DEFAULT_LOGGER_NAME)
 logging.basicConfig(
     filename=f"{DEFAULT_LOGGER_NAME}.log",
     filemode="w",  # use "a" to collect all in a session, use "w" to overwrite
     format="%(levelname)s %(asctime)s %(message)s",
-    datefmt="%Y-%m-%dT%H:%M:%S%z",
+    datefmt="%Y-%m-%dT%H:%M:%S.%f%z",
     encoding="utf-8",
     level=logging.DEBUG,
 )
@@ -54,23 +55,25 @@ logger.info(f"working_directory {os.getcwd()}")
 logger.info(f"microscope_directory {microscope_directory}")
 logger.info(f"target_directory {target_directory}")
 
+# load humans_and_companies.ods
+# load nion_data_additional_metadata.ods
+
 cnt = 0
 byte_size_processed = 0
 for root, dirs, files in os.walk(microscope_directory):
     for file in files:
         fpath = f"{root}/{file}".replace(os.sep * 2, os.sep)
-        fname = os.path.basename(fpath)
-        cnt += 1
+        # fname = os.path.basename(fpath)
+        # cnt += 1
         # if byte_size_processed >= (1 * 1024 * 1024 * 1024):  # incremental reporting
         #    byte_size_processed = 0
         # always attempt to hash the file first
         try:
-            logger.info(f"{cnt}, {fpath}, {fname}")
-            # stat = os.stat(fpath)
+            stat = os.stat(fpath)
             # byte_size_processed += stat.st_size
+            logger.info(f"{fpath}{SEPARATOR}{stat.st_size}")
         except Exception as e:
-            logger.warning(f"{fpath}____{e}")
-            continue
+            logger.warning(f"{fpath}{SEPARATOR}{e}")
 
 toc = datetime.datetime.now().timestamp()
 logger.info(f"{toc}")
