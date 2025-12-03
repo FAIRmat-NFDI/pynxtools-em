a697c029a7c8675dc9dd103e859d8e804525446b
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2025-12-03 17:08:04 +0100

    Initial commit, run-through specific code

---
+++ examples/ger_berlin_koch/ger_berlin_koch_batch_process.py
@@ -0,0 +1,27 @@
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""Functionalities for batch processing data."""
+
+# data should be stored in projects under some home directory
+# a batch queue processes projects in this HOME directory based on
+# a single coordinating OpenOffice sheet is used to inject
+# additional ELN-style "missing" data and configure the projects parsed
+# during this processing technology-partner specific content,
+# e.g., nion, JEOL, Zeiss, stuff is parsed using pynxtools-em to generate
+# one NeXus/HDF5 file per project
