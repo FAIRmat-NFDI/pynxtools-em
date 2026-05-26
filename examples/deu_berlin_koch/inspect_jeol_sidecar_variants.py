e983073c951f859e1f9d989b83ea9135ff4c4c21
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2026-05-26 18:25:12 +0200

    example how to check rigorously compliance with a formatting layout for text files, as it is typical in electron microscopy here exemplified for a JEOL instrument

---
+++ examples/deu_berlin_koch/inspect_jeol_sidecar_variants.py
@@ -0,0 +1,104 @@
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""Distinguish different formatting variants for JEOL plain text metadata files."""
+
+# test code for working with data from jeol_data before putting it into pynxtools-em
+# import magic  # ancient but most robust print(magic.from_file(path, mime=True))
+import os
+import re
+
+import puremagic  # modern, pythonic replacement but not that covering
+
+from pynxtools_em.examples.get_sha256_of_directories import SEPARATOR
+
+# JEOL, Hannah/20210225_CsPbBrI40Big_TEMIsrael/1.txt
+layout_one: list[str] = [
+    r"^\$CM_FORMAT (?:\r\n|\n)$",
+    r"^\$CM_VERSION (?:\r\n|\n)$",
+    r"^\$CM_COMMENT  (?:\r\n|\n)$",
+    r"^\$CM_DATE ([1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])/\d{4}(?:\r\n|\n)$",
+    r"^\$CM_TIME (0[1-9]|1[0-9]|2[0-3]):(0[1-9]|1[0-9]|2[0-3]):(0[1-9]|1[0-9]|2[0-3]) (AM|PM)(?:\r\n|\n)$",
+    r"^\$CM_OPERATOR [^\r\n]+(?:\r\n|\n)$",
+    r"^\$CM_INSTRUMENT JEM-2200FS(?:\r\n|\n)$",
+    r"^\$CM_NAME Specimen(?:\r\n|\n)$",
+    r"^\$CM_FRAME_SIZE \d+ \d+(?:\r\n|\n)$",
+    r"^\$CM_DATA_BIT 8(?:\r\n|\n)$",
+    r"^\$CM_EFECT_BIT 8(?:\r\n|\n)$",
+    r"^\$CM_ACCEL_VOLT 200(?:\r\n|\n)$",
+    r"^\$CM_MAG \d+(?:\r\n|\n)$",
+    r"^\$CM_SIGNAL TEM(?:\r\n|\n)$",
+    r"^\$\$EM_PIXELSPERMETER_X (?:\d+(?:\.\d*)?|\.\d+)(?:\r\n|\n)$",
+    r"^\$\$EM_PIXELSPERMETER_Y (?:\d+(?:\.\d*)?|\.\d+)(?:\r\n|\n)$",
+]
+
+
+def does_file_conform_with_layout(path: str, layout: list[str]) -> bool:
+    """Check if path is a text file and if so follows the specific line-by-line layout as defined in layout."""
+    conforms: bool = True
+    # if magic.from_file(path, mime=True) == "text/plain":  # libmagic alternative but outdated compared to
+    if puremagic.from_file(path) == ".txt":
+        with open(path) as fp:
+            n_lines_layout: int = len(layout)
+            txt = fp.readlines()
+            for idx, line in enumerate(txt):
+                if idx < n_lines_layout:
+                    if not re.fullmatch(layout[idx], line):
+                        conforms = False
+                        break
+                else:
+                    conforms = False
+                    break
+    return conforms
+
+
+def inspect_jeol_metadata(root_path: str, prefix: str, write: bool = True) -> None:
+    """Recurse all files in root_path, if metadata sidecar file, check if matches any known formatting."""
+
+    if write:
+        # summary: dict[str, list[str]] = {"layout_one": []}
+        for root, dirs, files in os.walk(root_path):
+            for name in files:
+                path = os.path.join(root, name)
+                if path.lower().endswith(".txt"):
+                    for name, layout in [("1", layout_one)]:
+                        status = does_file_conform_with_layout(path, layout_one)
+                        if not status:
+                            print(f"{name}, {path}")
+        # with open(f"{prefix}.directories.csv", "w") as fp:
+        #     fp.write("\n".join(csv_directories))
+
+
+def main():
+    # e.g. call via
+    # `python3 inspect_jeol_metadata_variants.py /microscope_data microscope_data`
+    if len(sys.argv) > 1:
+        root_path = sys.argv[1]
+    else:
+        root_path = "."
+
+    if len(sys.argv) > 2:
+        prefix = sys.argv[2]
+    else:
+        prefix = "inspect_jeol_metadata"
+
+    inspect_jeol_metadata(root_path, prefix)
+
+
+if __name__ == "__main__":
+    main()
