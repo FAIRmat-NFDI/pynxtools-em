315f0f977bf9b90d6dca2f56f34d2109e11a2a2b
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2026-05-26 13:08:43 +0200

    code for inspecting a local collection of directories and files

---
+++ examples/deu_berlin_koch/inspect_microscope_database.py
@@ -0,0 +1,48 @@
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""Inspect a directory for all its files and folders."""
+
+import os
+import sys
+
+
+def inspect(root_path: str, output: str = f"in"):
+    """Recursively list all directories and files in root_path."""
+    csv = []
+    for root, dirs, files in os.walk(root_path):
+        for name in dirs + files:
+            csv.append(name)
+            # csv.append(os.path.join(root, name))
+
+    with open(f"{output}.csv", "w") as fp:
+        fp.write("\n".join(csv))
+
+
+def main():
+    root = sys.argv[1] if len(sys.argv) > 1 or sys.argv[1] == "" else "."
+    name = (
+        sys.argv[2]
+        if len(sys.argv) > 2 or sys.argv[2] == ""
+        else "inspect_microscope_database"
+    )
+    inspect(root, name)
+
+
+if __name__ == "__main__":
+    main()
