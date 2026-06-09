1a9fb4be59ee8f589c6e98023d9b09501121f310
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2026-06-09 12:19:56 +0200

    remove NeXus/MTex parser as this one has been updated and will be maintained via the pynxtools-microstructure plugin

--- src/pynxtools_em/reader.py
+++ src/pynxtools_em/reader.py
@@ -49,7 +49,6 @@ from pynxtools_em.parsers.image_tiff_point_electronic import PointElectronicTiff
 from pynxtools_em.parsers.image_tiff_tescan import TescanTiffParser
 from pynxtools_em.parsers.image_tiff_tfs import TfsTiffParser
 from pynxtools_em.parsers.image_tiff_zeiss import ZeissTiffParser
-from pynxtools_em.parsers.nxs_mtex import NxEmNxsMtexParser
 from pynxtools_em.parsers.nxs_nion import NionProjectParser
 from pynxtools_em.parsers.oasis_config import NxEmNomadOasisConfigParser
 from pynxtools_em.parsers.oasis_eln import NxEmNomadOasisElnSchemaParser
@@ -160,7 +159,6 @@ class EMReader(BaseReader):
                 ProtochipsPngSetParser,
                 RsciioVeloxParser,
                 RsciioGatanParser,
-                NxEmNxsMtexParser,
                 NionProjectParser,
                 FeiLegacyTiffParser,
             ]
